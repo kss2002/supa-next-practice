69f6e50b987910c5dd7c23417df18c104050d256
seong su kim <know.warehouse02@gmail.com>
2025-07-15 00:04:37 +0900

    users

---
+++ src/app/users/update/UserUpdateClient.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { userState } from '@/app/recoil/atoms';
+import { useRecoilState } from 'recoil';
+
+export default function UserUpdateClient() {
+  const [user, setUser] = useRecoilState(userState);
+
+  return (
+    <>
+      <div>
+        <h1>update user page</h1>
+        <input
+          type="email"
+          placeholder="이메일을 작성해주세요!"
+          value={user.email}
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev, email: e.target.value }))
+          }
+        />
+        <input
+          type="text"
+          placeholder="이름을 넣어주세요!"
+          value={user.name}
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+      </div>
+    </>
+  );
+}
